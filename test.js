7ac34e0029a43fe904d99139affe1af67ce24714
leo <leosmilence@163.com>
2017-04-14 13:20:11 +0800

    123

---
+++ test.js
@@ -0,0 +1,20 @@
+
+
+class CC{
+    constructor(){
+        this.b=1;
+        this.a=this.b;
+    }
+}
+
+let aa=new CC();
+
+console.log(aa);
+
+
+let test={
+    a:a.b,
+    b:1
+}
+
+console.log(test);
